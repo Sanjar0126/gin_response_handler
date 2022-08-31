66be885e43de5a65fcfaa2dc2046de3604a030b7
Sanjar0126 <sanjar.niyozov.0126@gmail.com>
2022-08-31 23:42:11 +0500

    fix: change msg format

--- main.go
+++ main.go
@@ -3,26 +3,45 @@ package gin_response_handler
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
 
-type bodyLogWriter struct {
+type bodyWriter struct {
 	gin.ResponseWriter
 	body *bytes.Buffer
 }
 
-func (w bodyLogWriter) Write(b []byte) (int, error) {
+type responseBody struct {
+	message string `json:"message,omitempty"`
+	code    string `json:"code,omitempty"`
+}
+
+func (w bodyWriter) Write(b []byte) (int, error) {
 	w.body.Write(b)
 	return w.ResponseWriter.Write(b)
 }
 
 func GinResponseMiddleware(c *gin.Context) {
-	blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
-	c.Writer = blw
+	bw := &bodyWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+	c.Writer = bw
 	c.Next()
+
 	statusCode := c.Writer.Status()
+	path := c.FullPath()
+	method := c.Request.Method
+	proto := c.Request.Proto
+
+	reqBody, _ := ioutil.ReadAll(c.Request.Body)
+
 	if statusCode >= 400 {
-		fmt.Println("Response body: " + blw.body.String())
+		msg := fmt.Sprintf("%s %s %s\nResponse: %s", method, path, proto, bw.body.String())
+
+		if len(reqBody) > 0 {
+			msg = fmt.Sprintf("%s\nRequest: %s", msg, string(reqBody))
+		}
+
+		fmt.Println(msg)
 	}
 }
