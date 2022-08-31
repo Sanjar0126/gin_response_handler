564821cf0d27a137cd646295d4ccd001983c7f51
Sanjar0126 <sanjar.niyozov.0126@gmail.com>
2022-08-31 22:52:45 +0500

    init commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package gin_response_handler
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type bodyLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func GinResponseMiddleware(c *gin.Context) {
+	blw := &bodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+	c.Writer = blw
+	c.Next()
+	statusCode := c.Writer.Status()
+	if statusCode >= 400 {
+		fmt.Println("Response body: " + blw.body.String())
+	}
+}
